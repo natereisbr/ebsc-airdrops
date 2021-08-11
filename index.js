a7f8104ff8926d74cc60b40c06f85df64e5ac3f3
natereisbr <natereisbr@gmail.com>
2021-08-11 19:23:11 -0300

    server fix

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const jsdom = require("jsdom");
 const bodyParser = require('body-parser')
 const { JSDOM } = jsdom;
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000
 const path = require('path');
 const fs = require('fs');
 
